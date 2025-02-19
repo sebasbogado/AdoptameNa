a1094a3987d06b861f0e035c08d9d72b6e6100d8
Ivan F <jf561428@gmail.com>
2025-02-19 17:28:16 -0300

    feat: add validation annotations for email and password in login and registration requests

--- src/main/java/com/fiuni/adoptamena/auth/LoginRequest.java
+++ src/main/java/com/fiuni/adoptamena/auth/LoginRequest.java
@@ -4,15 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequest {
     @NotBlank(message = "El email no puede estar vacío")
+    @Email(message = "Formato de email incorrecto")
+    @Email
     String email;
+
     @NotBlank(message = "La contrasena no puede estar vacio")
+    @Size(min = 6, message = "La contrasena debe tener al menos 6 caracteres")
     String password;
 }
-
--- src/main/java/com/fiuni/adoptamena/auth/RegisterRequest.java
+++ src/main/java/com/fiuni/adoptamena/auth/RegisterRequest.java
@@ -1,5 +1,8 @@
 package com.fiuni.adoptamena.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +13,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+
+    @NotBlank(message = "El email no puede estar vacío")
+    @Email(message = "Formato de email incorrecto")
     String email;
+
+    @NotBlank(message = "La contraseña no puede estar vacía")
+    @Size(min = 6, message = "La contrasena debe tener al menos 6 caracteres")
     String password;
 }
