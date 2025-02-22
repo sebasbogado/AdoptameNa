87b2b25cb6c58ecd6cdbafd9a7e08a3f9944291a
jorgef29 <jorgefigueredo2001@gmail.com>
2025-02-22 00:30:00 -0300

    add: ProfileController

---
+++ src/main/java/com/fiuni/adoptamena/api/controller/profile/ProfileController.java
@@ -0,0 +1,10 @@
+package com.fiuni.adoptamena.api.controller.profile;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/profiles")
+public class ProfileController {
+
+}
