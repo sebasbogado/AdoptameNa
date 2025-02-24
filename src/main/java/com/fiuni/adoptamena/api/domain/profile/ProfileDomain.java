f5b42fc9f51a985b4a63a5501946e006bb072a90
jorgef29 <jorgefigueredo2001@gmail.com>
2025-02-24 06:18:43 -0300

    fix: type of birthdate

--- src/main/java/com/fiuni/adoptamena/api/domain/profile/ProfileDomain.java
+++ src/main/java/com/fiuni/adoptamena/api/domain/profile/ProfileDomain.java
@@ -3,6 +3,7 @@ import lombok.*;
 import jakarta.persistence.*;
 import com.fiuni.adoptamena.api.domain.base.BaseDomain;
 import java.io.Serial;
+import java.util.Date;
 
 import com.fiuni.adoptamena.api.domain.user.UserDomain;
 
@@ -45,7 +46,7 @@ public class ProfileDomain implements BaseDomain {
     private String gender;
 
     @Column(name="date_birthdate")
-    private String birthdate;
+    private Date birthdate;
 
     @Column(name="str_document")
     private String document;
