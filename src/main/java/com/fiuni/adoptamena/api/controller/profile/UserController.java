e0c0b16a8fd1ae5f0799d10e5e86ad115f60b5c4
jorgef29 <jorgefigueredo2001@gmail.com>
2025-02-25 11:02:01 -0300

    add: user controller

--- src/main/java/com/fiuni/adoptamena/api/controller/profile/UserController.java
+++ src/main/java/com/fiuni/adoptamena/api/controller/profile/UserController.java
@@ -1,18 +1,35 @@
 package com.fiuni.adoptamena.api.controller.profile;
 
+import com.fiuni.adoptamena.api.dto.profile.ProfileDTO;
+import com.fiuni.adoptamena.api.service.profile.IProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
+
+    @Autowired
+    private IProfileService profileService;
+
     @GetMapping("/profiles")
     public String getOk() {
         return "OK";
     }
+    @PostMapping("/{id}/profile")
+    public ResponseEntity<ProfileDTO> postMethodName(@RequestBody ProfileDTO profile, @PathVariable Integer id) {
+        log.info("Llega al controller, {}", profile);
+        ProfileDTO result = profileService.saveProfile(profile, id);
+        return new ResponseEntity<>(result,HttpStatus.CREATED);
+    }
+    @GetMapping("/{id}/profile")
+    public ResponseEntity<ProfileDTO> getMethodName(@PathVariable Integer id) {
+        ProfileDTO result = profileService.getById(id);
+        return new ResponseEntity<>(result,HttpStatus.OK);
+    }
     
 }
-/*
- * 
- * 
- * 
- */
