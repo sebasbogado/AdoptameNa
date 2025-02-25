90c5c19e14d48d1ad7d8e346d1d399807ee2904a
jorgef29 <jorgefigueredo2001@gmail.com>
2025-02-25 11:00:38 -0300

    add: @data

--- src/main/java/com/fiuni/adoptamena/api/dto/base/BaseDTO.java
+++ src/main/java/com/fiuni/adoptamena/api/dto/base/BaseDTO.java
@@ -1,11 +1,9 @@
 package com.fiuni.adoptamena.api.dto.base;
 
 import java.io.Serializable;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
 public abstract class BaseDTO implements Serializable {
     private static final long serialVersionUID = 1L;
     private Integer id;
