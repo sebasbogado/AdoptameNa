aebc626fd92bb344eb2893557bde05f3979d07e9
jorgef29 <jorgefigueredo2001@gmail.com>
2025-02-24 23:45:10 -0300

    add: find by id and deleted false

--- src/main/java/com/fiuni/adoptamena/api/dao/profile/IProfileDao.java
+++ src/main/java/com/fiuni/adoptamena/api/dao/profile/IProfileDao.java
@@ -3,9 +3,8 @@ package com.fiuni.adoptamena.api.dao.profile;
 import com.fiuni.adoptamena.api.domain.profile.ProfileDomain;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 @Repository
 public interface IProfileDao extends JpaRepository<ProfileDomain, Integer> {
-    //Optional<ProfileDomain> findByEmail(String email);
-    // Métodos adicionales si es necesario
-    
+    Optional<ProfileDomain> findByIdAndDeletedFalse(Integer id);
 }
