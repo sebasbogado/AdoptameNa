2cebf0ba21e85e467e9778679c0b84e0cd82851d
jorgef29 <jorgefigueredo2001@gmail.com>
2025-02-25 11:46:50 -0300

    update: use CrudRepository

--- src/main/java/com/fiuni/adoptamena/api/dao/profile/IProfileDao.java
+++ src/main/java/com/fiuni/adoptamena/api/dao/profile/IProfileDao.java
@@ -1,10 +1,11 @@
 package com.fiuni.adoptamena.api.dao.profile;
 
 import com.fiuni.adoptamena.api.domain.profile.ProfileDomain;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 @Repository
-public interface IProfileDao extends JpaRepository<ProfileDomain, Integer> {
+public interface IProfileDao extends CrudRepository<ProfileDomain, Integer> {
     Optional<ProfileDomain> findByIdAndIsDeletedFalse(Integer id);
 }
