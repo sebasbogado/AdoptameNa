bec4a7fee3ee63c6d0d85a4a277c49899876a068
jorgef29 <jorgefigueredo2001@gmail.com>
2025-02-25 11:46:23 -0300

    update: methods

--- src/main/java/com/fiuni/adoptamena/api/service/profile/IProfileService.java
+++ src/main/java/com/fiuni/adoptamena/api/service/profile/IProfileService.java
@@ -3,11 +3,9 @@ package com.fiuni.adoptamena.api.service.profile;
 import com.fiuni.adoptamena.api.dto.profile.ProfileDTO;
 public interface IProfileService {
     //save
-    ProfileDTO saveProfile(ProfileDTO profile, Integer userId);
+    void saveProfile(Integer userId);
     //update
     ProfileDTO updateProfile(ProfileDTO profile);
     //get by id
     ProfileDTO getById(Integer id);
-    //delete
-    void deleteProfile(Integer id);
 }
