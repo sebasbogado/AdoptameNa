48f4b9f6ddbb92d4d57712370f18020187955484
Rodrigo Maidana <114775631+romaidana29@users.noreply.github.com>
2025-02-26 10:06:00 -0300

    update: change from JpaRepository to CrudRepository in Daos

--- src/main/java/com/fiuni/adoptamena/api/dao/user/IVerificationTokenDao.java
+++ src/main/java/com/fiuni/adoptamena/api/dao/user/IVerificationTokenDao.java
@@ -2,11 +2,11 @@ package com.fiuni.adoptamena.api.dao.user;
 
 import com.fiuni.adoptamena.api.domain.user.UserDomain;
 import com.fiuni.adoptamena.api.domain.user.VerificationTokenDomain;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface IVerificationTokenDao extends JpaRepository<VerificationTokenDomain, Integer> {
+public interface IVerificationTokenDao extends CrudRepository<VerificationTokenDomain, Integer> {
     Optional<VerificationTokenDomain> findByToken(String token);
 
     // FindByUser
