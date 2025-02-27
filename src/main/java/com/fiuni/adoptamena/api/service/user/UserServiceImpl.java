6075a3de78e3999e429451db247743e6b097b644
jorgef29 <jorgefigueredo2001@gmail.com>
2025-02-26 21:24:44 -0300

    fix: delete profile after user

--- src/main/java/com/fiuni/adoptamena/api/service/user/UserServiceImpl.java
+++ src/main/java/com/fiuni/adoptamena/api/service/user/UserServiceImpl.java
@@ -3,8 +3,10 @@ package com.fiuni.adoptamena.api.service.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fiuni.adoptamena.api.domain.profile.ProfileDomain;
 import com.fiuni.adoptamena.api.domain.user.UserDomain;
 import com.fiuni.adoptamena.exception_handler.exceptions.ResourceNotFoundException;
+import com.fiuni.adoptamena.api.dao.profile.IProfileDao;
 import com.fiuni.adoptamena.api.dao.user.IUserDao;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,11 +18,20 @@ public class UserServiceImpl implements IUserService {
     @Autowired
     private IUserDao userDao;
 
+    @Autowired
+    private IProfileDao profileDao;
+
     @Override
     public void deleteUser(Integer id) {
         UserDomain domain = userDao.findByIdAndIsDeletedFalse(id).orElseThrow(()-> 
             new ResourceNotFoundException("Usuario no encontrado"));
         domain.setIsDeleted(true);
+        //delete profile
+        ProfileDomain profile = profileDao.findByIdAndIsDeletedFalse(id).orElseThrow(()-> 
+            new ResourceNotFoundException("Perfil no encontrado"));
+        profile.setIsDeleted(true);
+        
+        profileDao.save(profile);
         userDao.save(domain);
         log.info("Usuario eliminado: {}", domain);
     }
