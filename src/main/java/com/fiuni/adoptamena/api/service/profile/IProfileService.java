1d7e86c296bb8fc0c6aa41174dee6a2c64feaa65
jorgef29 <jorgefigueredo2001@gmail.com>
2025-02-26 22:15:41 -0300

    fix: extends IBaseService

--- src/main/java/com/fiuni/adoptamena/api/service/profile/IProfileService.java
+++ src/main/java/com/fiuni/adoptamena/api/service/profile/IProfileService.java
@@ -1,11 +1,8 @@
 package com.fiuni.adoptamena.api.service.profile;
 
 import com.fiuni.adoptamena.api.dto.profile.ProfileDTO;
-public interface IProfileService {
-    //save
-    void saveProfile(Integer userId);
-    //update
-    ProfileDTO updateProfile(Integer id, ProfileDTO profile);
-    //get by id
-    ProfileDTO getById(Integer id);
+import com.fiuni.adoptamena.api.service.base.IBaseService;
+import com.fiuni.adoptamena.api.domain.profile.ProfileDomain;
+public interface IProfileService extends IBaseService<ProfileDomain,ProfileDTO> {
+
 }
