4b3cba1dd48859cc30bb654f8a7c65c7e8a7137b
jorgef29 <jorgefigueredo2001@gmail.com>
2025-02-26 21:22:18 -0300

    add: endpoint to delete user

--- src/main/java/com/fiuni/adoptamena/api/controller/profile/UserController.java
+++ src/main/java/com/fiuni/adoptamena/api/controller/profile/UserController.java
@@ -2,12 +2,13 @@ package com.fiuni.adoptamena.api.controller.profile;
 
 import com.fiuni.adoptamena.api.dto.profile.ProfileDTO;
 import com.fiuni.adoptamena.api.service.profile.IProfileService;
+import com.fiuni.adoptamena.api.service.user.IUserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import lombok.extern.slf4j.Slf4j;
-
 @RestController
 @RequestMapping("/users")
 @Slf4j
@@ -15,6 +16,9 @@ public class UserController {
 
     @Autowired
     private IProfileService profileService;
+
+    @Autowired
+    private IUserService userService;
 
     @GetMapping("/profiles")
     public String getOk() {
@@ -30,5 +34,10 @@ public class UserController {
         log.info("llega a controller");
         ProfileDTO result = profileService.updateProfile(id, profile);
         return new ResponseEntity<>(result,HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMethodName(@PathVariable Integer id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
