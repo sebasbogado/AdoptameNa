54810ca8a75a06b0ad865a1b0e5381c800f74f79
Rodrigo Maidana <114775631+romaidana29@users.noreply.github.com>
2025-02-28 09:03:01 -0300

    feat: agregar campo fullName a RegisterRequest y establecerlo en el perfil del usuario

--- src/main/java/com/fiuni/adoptamena/auth/RegisterRequest.java
+++ src/main/java/com/fiuni/adoptamena/auth/RegisterRequest.java
@@ -14,6 +14,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RegisterRequest {
 
+    @NotBlank(message = "El nombre no puede estar vacío")
+    String fullName;
+
     @NotBlank(message = "El email no puede estar vacío")
     @Email(message = "Formato de email incorrecto")
     String email;
