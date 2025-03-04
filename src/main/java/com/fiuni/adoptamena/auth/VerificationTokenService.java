9952a2aa9997a4ffd0564126a82f43f45fce963f
Rodrigo Maidana <ro-maidana@hotmail.com>
2025-03-04 14:54:49 -0300

    fix: removed unused imports

--- src/main/java/com/fiuni/adoptamena/auth/VerificationTokenService.java
+++ src/main/java/com/fiuni/adoptamena/auth/VerificationTokenService.java
@@ -12,7 +12,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
