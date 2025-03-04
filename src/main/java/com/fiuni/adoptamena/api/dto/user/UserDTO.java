7c6b7cf7e673573662ccf8651bd2810feddcf8e6
Rodrigo Maidana <114775631+romaidana29@users.noreply.github.com>
2025-03-03 22:06:12 -0300

    fix: extender UserDTO de BaseDTO y agregar serialVersionUID

--- src/main/java/com/fiuni/adoptamena/api/dto/user/UserDTO.java
+++ src/main/java/com/fiuni/adoptamena/api/dto/user/UserDTO.java
@@ -2,14 +2,17 @@ package com.fiuni.adoptamena.api.dto.user;
 
 import java.util.Date;
 
+import com.fiuni.adoptamena.api.dto.base.BaseDTO;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class UserDTO {
-
-    private Integer id;
+@EqualsAndHashCode(callSuper = false)
+public class UserDTO extends BaseDTO {
+    private static final long serialVersionUID = 1L;
 
     @NotBlank(message = "El email es requerido")
     @Email(message = "El email no es válido")
