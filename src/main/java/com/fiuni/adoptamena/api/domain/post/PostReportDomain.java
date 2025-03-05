1032ff248baf29104859a911128ff314e2584ade
WMartinez93 <willian.martinez@fiuni.edu.py>
2025-03-04 22:04:19 -0300

    Se agrego implementacion del CRUD de PostReports.

--- src/main/java/com/fiuni/adoptamena/api/domain/post/PostReportDomain.java
+++ src/main/java/com/fiuni/adoptamena/api/domain/post/PostReportDomain.java
@@ -1,12 +1,14 @@
 package com.fiuni.adoptamena.api.domain.post;
 
 import com.fiuni.adoptamena.api.domain.base.BaseDomain;
+import com.fiuni.adoptamena.api.domain.user.UserDomain;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serial;
+import java.util.Date;
 
 @Entity
 @Table(name = "post_reports")
@@ -22,4 +24,29 @@ public class PostReportDomain implements BaseDomain {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_post_report", nullable = false, unique = true)
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user", nullable = false)
+    private UserDomain user;
+
+    @ManyToOne
+    @JoinColumn(name = "id_post")
+    private PostDomain post;
+
+    @ManyToOne
+    @JoinColumn(name = "id_reason")
+    private ReportReasonsDomain reportReasons;
+
+    @Column(name = "str_description")
+    private String description;
+
+    @Column(name = "date_report_date")
+    private Date reportDate;
+
+    @Column(name = "str_status")
+    private String status;
+
+    @Column(name = "boolean_deleted")
+    private Boolean isDeleted;
+
 }
